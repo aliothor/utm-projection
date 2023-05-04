9bd42e59abbc8b2a0ed1f3dbac2dd4a1e62bc8ec
taojianhang <1811783973@qq.com>
2023-05-04 14:25:50 +0800

    1、补充说明信息

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# utm坐标转换工具
+# UTM 坐标转换工具
+
+```typescript
+import { fromLatLon, toLatLon } from "utm-projection";
+const x = 132.93249308340933;
+const y = 45.734655020259765;
+
+/**
+ * 经纬度转UTM
+ * res = {
+  easting: 339145.47167257813,
+  northing: 5066645.291212298,
+  zoneNum: 53,
+  zoneLetter: 'T'
+}
+ */
+const res = fromLatLon(y, x);
+
+/**
+ * UTM转经纬度
+ * res2 = { latitude: 45.73465500583696, longitude: 132.93249254427462 }
+ */
+const res2 = toLatLon(res.easting, res.northing, res.zoneNum);
+```
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,14 @@
-import { describe, it } from 'vitest'
-import { fromLatLon, toLatLon } from '../src'
+import { describe, it } from "vitest";
+import { fromLatLon, toLatLon } from "../src";
 
-describe('wgs 84', () => {
-    it('test1', () => {
-        // 339145 5.06665e+06
-        const x = 132.93249308340933;
-        const y = 45.734655020259765;
-        const res = fromLatLon(y, x)
-        console.log(res);
-
-        const res2 = toLatLon(res.easting, res.northing, res.zoneNum, null, true)
-        console.log(res2);
-
-    })
-})
+describe("wgs 84", () => {
+  it("test1", () => {
+    // 339145 5.06665e+06
+    const x = 132.93249308340933;
+    const y = 45.734655020259765;
+    const res = fromLatLon(y, x);
+    console.log(res);
+    const res2 = toLatLon(res.easting, res.northing, res.zoneNum);
+    console.log(res2);
+  });
+});
